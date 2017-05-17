76b3a2d0a288066acc0a26064d6d066eaa870526
arfullight <tmimvhuang@gmail.com>
2017-05-17 21:42:21 +0800

    add devise gem

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,5 @@
 class LinksController < ApplicationController
+  before_action :authenticate_user!, expect: :show
   before_action :set_link, only: [:edit, :update, :destroy]
 
   def index
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def index
+    render plain: 'Welcome to Coding Alpaca Site!'
+  end
+end
