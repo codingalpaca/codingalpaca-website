415469c26fedc203b351859ca2ba5b05c31170db
arfullight <tmimvhuang@gmail.com>
2017-05-17 14:44:10 +0800

    finish new, create, destroy

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,41 @@
 class LinksController < ApplicationController
+  before_action :set_link, only: [:edit, :update, :destroy]
+
+  def index
+    @links = Link.all
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new link_params
+
+    if @link.save
+      redirect_to links_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @link.destroy
+    redirect_to links_path
+  end
+
+  private
+  def link_params
+    params.require(:link).permit!
+  end
+
+  def set_link
+    @link = Link.find params[:id]
+  end
 end
