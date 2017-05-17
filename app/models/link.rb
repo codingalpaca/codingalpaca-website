a18ede6c6a8753a03e6320efc1daffcb5951da3d
arfullight <tmimvhuang@gmail.com>
2017-05-17 20:41:21 +0800

    add validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,5 @@
 class Link < ApplicationRecord
+  validates :title, presence: true
+  validates :target_url, presence: true,
+            format: { with: /\Ahttp[s]?:\/\//, message: 'should start with http://' }
 end
