7479ee5aab6daecb91d9cf092a4749dfcd9b4285
arfullight <tmimvhuang@gmail.com>
2017-05-18 17:42:57 +0800

    create user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518085424) do
+ActiveRecord::Schema.define(version: 20170518093843) do
 
   create_table "links", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170518085424) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "pixel_event_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
